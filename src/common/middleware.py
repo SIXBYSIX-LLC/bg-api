45ab2ad86805841e24e8ef25298a31403054095d
Abhinav <in.abhi9@gmail.com>
2015-06-10 01:02:16 +0530

    Add middleware to send extra information in header
    - RequestId
    - Git commit hash
    - Runtime
    - Host name
    - Content length

---
+++ src/common/middleware.py
@@ -0,0 +1,36 @@
+import uuid
+import os
+import socket
+
+from django.utils import timezone
+
+
+# Getting branch and commit hash, we want to cache these to so declaring the here
+_p = os.popen('git name-rev --name-only $(git rev-parse HEAD)')
+branch = _p.read().replace('tags/', '').replace('^0', '').strip()
+commit_hash = os.popen('git rev-parse HEAD').read().strip()
+
+
+class HeadInfoMiddleware(object):
+    """
+    Adds additional headers to response
+    """
+    request_id = None
+    start_time = None
+
+    def process_request(self, request):
+        self.start_time = timezone.now()
+        self.request_id = uuid.uuid4().hex
+        request.id = self.request_id
+
+    def process_response(self, request, response):
+        try:
+            response['X-Runtime'] = (timezone.now() - self.start_time).total_seconds()
+            response['X-Request-Id'] = self.request_id
+        except TypeError:
+            pass
+        response['X-Version'] = '%s#%s' % (branch, commit_hash)
+        response['X-Served-By'] = socket.gethostname()
+        response['Content-Length'] = len(response.content)
+
+        return response
