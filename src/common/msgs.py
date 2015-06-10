b1678d9765dc29a44f18fbd6547de5ff36ef951d
Abhinav <in.abhi9@gmail.com>
2015-06-11 01:31:14 +0530

    Include error code to msgs

--- src/common/msgs.py
+++ src/common/msgs.py
@@ -1,3 +1,3 @@
 from django.utils.translation import ugettext as _
 
-ERR_UNEXPECTED = _("Oops, something went wrong! We'll fix it soon.")
+ERR_UNEXPECTED = _("Oops, something went wrong! We'll fix it soon."), -1
