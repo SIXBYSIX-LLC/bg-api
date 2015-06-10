ecb1d070b4a811ee027e41769a5e6cc15c83f111
Abhinav <in.abhi9@gmail.com>
2015-06-11 01:30:13 +0530

    Override APIException to accommodate error_code

---
+++ src/common/errors.py
@@ -0,0 +1,11 @@
+from rest_framework import exceptions
+
+
+class APIException(exceptions.APIException):
+    def __init__(self, detail=None, error_code=-1):
+        super(APIException, self).__init__(detail=detail)
+        self.error_code = error_code
+
+
+class ValidationError(APIException):
+    status_code = 400
