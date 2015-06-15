c7d6c2dd31b56a4db960e326cb3d6a86f414882d
Abhinav <in.abhi9@gmail.com>
2015-06-16 01:29:12 +0530

    Added city database

---
+++ src/system/views.py
@@ -0,0 +1,29 @@
+from rest_framework import decorators
+from rest_framework.response import Response
+from cities.models import Country, Region, City
+
+from common import viewsets
+
+
+class CitiesViewSet(viewsets.GenericViewSet):
+    queryset = Country.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        data = {country.id: country.name for country in Country.objects.all()}
+
+        return Response(data)
+
+    @decorators.detail_route(methods=['GET'])
+    def regions(self, request, *args, **kwargs):
+        c_id = kwargs.get('pk')
+        data = {region.id: region.name for region in Region.objects.filter(country_id=c_id)}
+
+        return Response(data)
+
+
+    @decorators.detail_route(methods=['GET'], url_path='regions/(?P<rpk>[^/.]+)/cities')
+    def cities(self, request, *args, **kwargs):
+        r_id = kwargs.get('rpk')
+        data = {city.id: city.name for city in City.objects.filter(region_id=r_id)}
+
+        return Response(data)
