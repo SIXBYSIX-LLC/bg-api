78e8336a6934f72cbd89f8b68098ebfea7c62ac6
Abhinav <in.abhi9@gmail.com>
2015-06-21 01:26:54 +0530

    Add qty field to response

--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Count, Case, When, Value
+
 from common import viewsets
 from . import serializers, models
 
@@ -5,3 +7,8 @@ from . import serializers, models
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ProductSerializer
     queryset = models.Product.objects.all()
+
+    def get_queryset(self):
+        qs = super(ProductViewSet, self).get_queryset()
+        qs = qs.annotate(qty=Count(Case(When(inventory__is_active=True, then=Value(1)))))
+        return qs
