cf90327d81c1c21b4cf3ac92f2eb9f572851cd03
Abhinav <in.abhi9@gmail.com>
2015-06-21 00:23:02 +0530

    Change response.data that it gives actual data rather than meta

--- src/common/tests.py
+++ src/common/tests.py
@@ -1,9 +1,44 @@
+import json
+
 from django.conf import settings
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase, APIClient as _APIClient
 from rest_framework import status
 from django.core.management import call_command
 
 from usr import factories as usr_factories
+
+
+class APIClient(_APIClient):
+    def get(self, path, data=None, follow=False, **extra):
+        response = super(APIClient, self).get(path, data=data, follow=follow, **extra)
+        return self._fix_response(response)
+
+    def post(self, path, data=None, format=None, content_type=None, follow=False, **extra):
+        response = super(APIClient, self).post(path, data=data, format=format,
+                                               content_type=content_type, follow=follow, **extra)
+
+        return self._fix_response(response)
+
+    def put(self, path, data=None, format=None, content_type=None, follow=False, **extra):
+        response = super(APIClient, self).put(path, data=data, format=format,
+                                              content_type=content_type, follow=follow, **extra)
+        return self._fix_response(response)
+
+    def delete(self, path, data=None, format=None, content_type=None, follow=False, **extra):
+        response = super(APIClient, self).delete(path, data=data, format=format,
+                                                 content_type=content_type, follow=follow, **extra)
+        return self._fix_response(response)
+
+    def patch(self, path, data=None, format=None, content_type=None, follow=False, **extra):
+        response = super(APIClient, self).patch(path, data=data, format=format,
+                                                content_type=content_type, follow=follow, **extra)
+        return self._fix_response(response)
+
+    @classmethod
+    def _fix_response(cls, response):
+        response.meta = response.data
+        response.data = json.loads(response.content).get('data')
+        return response
 
 
 class TestCase(APITestCase):
