2650b3ab75bbdd0bc51751189ea3c3bd6d530eda
Abhinav <in.abhi9@gmail.com>
2015-06-23 19:49:26 +0530

    Address: Fix parent query lookups

--- src/usr/urls.py
+++ src/usr/urls.py
@@ -8,7 +8,7 @@ router.register(r'users', views.UserViewSet, base_name='user').register(r'addres
                                                                         views.AddressViewSet,
                                                                         base_name='user-address',
                                                                         parents_query_lookups=[
-                                                                            'user__address'])
+                                                                            'user'])
 
 password_reset_url = patterns('usr.views', url('^users/actions/password_reset', 'password_reset'))
 email_verify_url = patterns('usr.views', url('^users/actions/verify_email', 'verify_email'))
