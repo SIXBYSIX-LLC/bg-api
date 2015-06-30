460f393d558016eff2530f7be2c0266eb9cd4dc3
Abhinav <in.abhi9@gmail.com>
2015-06-30 14:54:10 +0530

    Add postgis to osinstall

--- osinstall.sh
+++ osinstall.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# The following packages have been tested with Ubuntu >= 14.04
+
 sudo apt-get install -y \
     libxml2-dev libxslt1-dev libffi-dev libssl-dev libpq-dev libpcre3 libpcre3-dev \
-    zlib1g-dev python-pip python-dev
+    zlib1g-dev python-pip python-dev postgis
