a8666002d8463281ac08408fede1650f783d614e
Abhinav <in.abhi9@gmail.com>
2015-06-30 13:50:49 +0530

    Removed libffi

--- osinstall.sh
+++ osinstall.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install -y \
     libxml2-dev libxslt1-dev libffi-dev libssl-dev libpq-dev libpcre3 libpcre3-dev \
-    zlib1g-dev python-pip python-dev libffi
+    zlib1g-dev python-pip python-dev
