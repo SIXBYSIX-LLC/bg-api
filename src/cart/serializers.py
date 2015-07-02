76e9f43b2130caefc48016eb3e8168f638e7d1bf
Abhinav <in.abhi9@gmail.com>
2015-07-03 00:05:18 +0530

    Update only location on cart

--- src/cart/serializers.py
+++ src/cart/serializers.py
@@ -7,12 +7,13 @@ from . import messages
 class CartSerializer(ModelSerializer):
     class Meta:
         model = Cart
+        read_only_fields = ('rental_products', 'user', 'is_active', 'total')
 
 
 class RentalProductSerializer(ModelSerializer):
     class Meta:
         model = RentalItem
-        read_only_fields = ('shipping_cost', 'subtotal', 'is_shippable')
+        read_only_fields = ('shipping_cost', 'subtotal', 'is_shippable', 'cost_breakup')
 
     def validate_product(self, value):
         product = value
@@ -23,9 +24,17 @@ class RentalProductSerializer(ModelSerializer):
     def validate(self, data):
         date_start = data.get('date_start')
         date_end = data.get('date_end')
+        product = data.get('product') or self.instance
 
         if date_start and date_end:
-            min_contract_period = data['product'].user.profile.settings['minimum_contract_period']
+            min_contract_period = product.user.profile.settings['minimum_contract_period']
             if (date_end - date_start).days < min_contract_period:
                 raise ValidationError(*messages.ERR_INVALID_END_DATE)
+
         return data
+
+    def update(self, instance, validated_data):
+        # Prevent product attribute from updating
+        validated_data.pop('product', None)
+
+        return super(RentalProductSerializer, self).update(instance, validated_data)
