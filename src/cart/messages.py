9c0ce73e5b0fe05e8045131b93d8a6b992061828
Abhinav <in.abhi9@gmail.com>
2015-07-03 00:06:48 +0530

    Update messages

--- src/cart/messages.py
+++ src/cart/messages.py
@@ -1,4 +1,4 @@
 from django.utils.translation import ugettext as _
 
 ERR_RENT_INVALID_PRODUCT = _('This product cannot be add as rental product'), 6001
-ERR_INVALID_END_DATE = _('End must be greater than minimum contract period'), 6002
+ERR_INVALID_END_DATE = _('End date must be greater than minimum contract period'), 6002
