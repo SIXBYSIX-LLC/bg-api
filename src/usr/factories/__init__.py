9641512e7a74ce1c2d50792a0437344733c1feb2
Abhinav <in.abhi9@gmail.com>
2015-07-08 00:52:09 +0530

    AddressFactory: Fix address name uniqueness

--- src/usr/factories/__init__.py
+++ src/usr/factories/__init__.py
@@ -75,7 +75,7 @@ class DeviceUserFactory(UserFactory):
 
 
 class AddressBaseFactory(factory.DictFactory):
-    name = factory.LazyAttribute(lambda x: fake.first_name())
+    name = factory.LazyAttribute(lambda x: fake.name() + fuzzy.FuzzyText().fuzz())
     address1 = factory.LazyAttribute(lambda x: fake.address())
     country = factory.LazyAttribute(lambda x: fake.cities_country().id)
     state = factory.LazyAttribute(lambda o: fake.cities_region(o.country).id)
