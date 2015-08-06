fb481defe34348f42cdccae144c2ba3f13e98b9c
Abhinav <in.abhi9@gmail.com>
2015-08-06 14:12:25 +0530

    Update permission

--- src/system/management/commands/syncperms.py
+++ src/system/management/commands/syncperms.py
@@ -67,7 +67,6 @@ class Command(BaseCommand):
             Permission.objects.get(codename='change_purchaseitem', content_type__app_label='cart'),
             Permission.objects.get(codename='delete_purchaseitem', content_type__app_label='cart'),
             # Order
-            Permission.objects.get(codename='add_order'),
             Permission.objects.get(codename='view_order'),
             Permission.objects.get(codename='change_order'),
             Permission.objects.get(codename='view_orderline'),
