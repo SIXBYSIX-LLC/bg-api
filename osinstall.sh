e473c6fd5e6db2ba38320d8107d6fd6205a94602
Abhinav <in.abhi9@gmail.com>
2015-08-27 20:49:33 +0530

    Update install os: Add wkhtmltopdf

--- osinstall.sh
+++ osinstall.sh
@@ -4,4 +4,4 @@
 
 sudo apt-get install -y \
     libxml2-dev libxslt1-dev libffi-dev libssl-dev libpq-dev libpcre3 libpcre3-dev \
-    zlib1g-dev python-pip python-dev postgis
+    zlib1g-dev python-pip python-dev postgis libfontconfig1 wkhtmltopdf
