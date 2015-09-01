4e63602f0289ff812e6f707f1e4808076d93138e
Abhinav <in.abhi9@gmail.com>
2015-09-01 22:18:40 +0530

    Define error codes

--- src/common/errors.py
+++ src/common/errors.py
@@ -6,6 +6,26 @@ Shared exception module
 from rest_framework import exceptions
 
 
+class Code(object):
+    _MULTIPLY = 1000
+
+    USR = 1 * _MULTIPLY
+    REVIEW = 2 * _MULTIPLY
+    CATALOG = 4 * _MULTIPLY
+    STATIC = 5 * _MULTIPLY
+    CART = 6 * _MULTIPLY
+    ORDER = 7 * _MULTIPLY
+    INVOICE = 8 * _MULTIPLY
+    TRANSACTION = 9 * _MULTIPLY
+    # CATEGORY = 6 * _MULTIPLY
+    #CHARGE = 6 * MULTIPLY
+    #GROUP = 6 * MULTIPLY
+    #INQUIRY = 6 * MULTIPLY
+    #PAYMENTGATEWAY = 6 * MULTIPLY
+    #SHIPPING = 6 * MULTIPLY
+    #SYSTEM = 6 * MULTIPLY
+
+
 class APIException(exceptions.APIException):
     """
     This exception catches by renderer and produce pretty output
@@ -55,4 +75,8 @@ class PaymentError(APIException):
 
 
 class CartError(APIException):
+    status_code = 422
+
+
+class ReviewError(APIException):
     status_code = 422
