94597763f1079b906908ae45f324ec5b76057fdf
Abhinav <in.abhi9@gmail.com>
2015-09-06 20:59:56 +0530

    Group: Update docstring

--- src/group/__init__.py
+++ src/group/__init__.py
@@ -8,6 +8,14 @@ The standard django group model has only name and permission fields, our require
 have ability to create his own groups and permission assigned to them. Hence I extended it.
 
 
+Default Groups
+--------------
+Whenever new system is deployed the following groups will be available:
+
+* ``Device``: From whatever the platform is accessible (like android, ios, browser) are under \
+these groups. Such user has very limited permissions (see management command).
+* ``User``: Typical user (buyer/seller) will be under these group
+
 Modules
 -------
 """
