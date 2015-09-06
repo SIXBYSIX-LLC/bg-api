0f3d4c79490e2779aedf41c030dbecbaeae447d8
Abhinav <in.abhi9@gmail.com>
2015-09-06 20:06:31 +0530

    System: Update docstring

--- src/system/__init__.py
+++ src/system/__init__.py
@@ -1,9 +1,19 @@
 """
+******
 System
-======
+******
 This module is aimed to provide some static information about system or that supported by system.
+So all system related models / methods should go here.
+
 For now this module provides following features
 
 * API view for cities: Country, region and city
+* Config storage class
+* Management commands
+* Cities fixtures for test cases
+
+
+Modules
+-------
 """
 default_app_config = 'system.apps.SystemConfig'
--- src/system/models.py
+++ src/system/models.py
@@ -1,3 +1,9 @@
+"""
+======
+Models
+======
+"""
+
 from django.db import models
 from djangofuture.contrib.postgres import fields as pg_fields
 
@@ -5,19 +11,30 @@ from common.models import BaseModel, DateTimeFieldMixin, BaseManager
 
 
 class ConfigManager(BaseManager):
+    """
+    Manager class for config
+    """
     def get_by_natural_key(self, k):
+        """
+        Returns config dict by key
+        """
         try:
             return self.get(id=k).config
         except Config.DoesNotExist:
             return {}
 
     def coreconf(self):
+        """
+        Shortcut method to retrieve core config dict
+        """
         return self.get_by_natural_key('core')
 
 
 class Config(BaseModel, DateTimeFieldMixin):
     """
-    (Cannot be deleted)
+    Class to store system wide configs
+
+    .. note:: Cannot be deleted
     """
     id = models.CharField(primary_key=True, max_length=50)
     config = pg_fields.JSONField(default={})
