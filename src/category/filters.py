8dc5fd90a5c67012c4e101719c7eb12c760b99f3
Abhinav <in.abhi9@gmail.com>
2016-03-30 12:01:45 +0530

    Category: Fix category parent null filter

--- src/category/filters.py
+++ src/category/filters.py
@@ -1,11 +1,15 @@
 import django_filters
 
+from common import helper
 from .models import Category
 
 
 class CategoryFilter(django_filters.FilterSet):
-    parent__isnull = django_filters.BooleanFilter(name="parent__isnull")
+    parent__isnull = django_filters.MethodFilter(action='filter_parent_isnull')
 
     class Meta:
         model = Category
         fields = ['parent__isnull', 'parent']
+
+    def filter_parent_isnull(self, queryset, value):
+        return queryset.filter(parent__isnull=helper.str2bool(value))
